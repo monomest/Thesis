6b3a17eac54aa384eef6e2857c18914e88c4c827
monomest <reneeylu29@gmail.com>
2021-04-07 16:20:04 +1000

    CU Kids - TDNN-HMM

---
+++ s5/local/cu_speed_perturb.sh
@@ -0,0 +1,7 @@
+# cu_speed_perturb.sh
+# Author: Renee Lu
+# About: This code speed perturbs the CU Kids Speech Corpus
+#        It randomly selects a warp factor between 0.9 to 1.1
+#        and applies that to each speech file in the corpus.
+
+
