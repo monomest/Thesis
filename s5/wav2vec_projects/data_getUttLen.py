94100a10357febc2f2ab0b0e398c50030f7771d0
monomest <reneeylu29@gmail.com>
2021-11-10 18:13:02 +1100

    Utt length eval myST-OGI-TLT17

--- s5/wav2vec_projects/data_getUttLen.py
+++ s5/wav2vec_projects/data_getUttLen.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Setting filepaths
 fp = "/srv/scratch/z5160268/2020_TasteofResearch/kaldi/egs/renee_thesis/s5/myST_local/THESIS_C/myST_data_test_light.csv"
-out_fp = "/srv/scratch/z5160268/2020_TasteofResearch/kaldi/egs/renee_thesis/s5/myST_local/THESIS_C/myST_data_test_uttlen.csv"
+out = "/srv/scratch/z5160268/2020_TasteofResearch/kaldi/egs/renee_thesis/s5/myST_local/THESIS_C/myST_data_test_uttlen"
 # Read in file
 df = pd.read_csv(fp, dtype=str)
 # Get total words
@@ -13,15 +13,19 @@ df['utterance_group'] = np.where((df['utterance_length']==1),
                             1, np.nan)
 df['utterance_group'] = np.where((df['utterance_length'] > 1) &
                                  (df['utterance_length'] < 6),
-                            5, df['utterance_group'])
+                            2, df['utterance_group'])
 df['utterance_group'] = np.where((df['utterance_length'] > 5) &
                                  (df['utterance_length'] < 11),
-                            10, df['utterance_group'])
+                            3, df['utterance_group'])
 df['utterance_group'] = np.where((df['utterance_length'] > 10) &
                                  (df['utterance_length'] < 21),
-                            20, df['utterance_group'])
+                            4, df['utterance_group'])
 df['utterance_group'] = np.where((df['utterance_length'] > 20),
-                            21, df['utterance_group'])
+                            5, df['utterance_group'])
 # Save to output file
+out_fp = out+".csv"
 df.to_csv(out_fp, index=False)
-
+for i in range(1,6):
+    out_fp = out+str(i)+".csv"
+    save_df = df[(df['utterance_group'] == i)]
+    save_df.to_csv(out_fp, index=False)
