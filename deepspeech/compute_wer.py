38143bc92fdfabdb0407c69d6d8a865a79081548
monomest <reneeylu29@gmail.com>
2022-05-05 22:58:23 +1000

    DeepSpeech child WER

--- deepspeech/compute_wer.py
+++ deepspeech/compute_wer.py
@@ -1,84 +1,39 @@
+#----------------------------------------------------------
 # compute_wer.py
-# Purpose: Compute WER of DeepSpeech inferences
+# Purpose: Compute WER given a dataframe of transcriptions
+#          and predictions
+# Based on source:
+# https://github.com/huggingface/datasets/tree/master/metrics/wer
 # Author: Renee Lu, 2022
+#----------------------------------------------------------
+
 
 # ------------------------------------------
 #          Importing libraries
 # ------------------------------------------
-
 # For WER
-from jiwer import wer
-
+from datasets import load_metric
+import pandas as pd
 # For printing filepath
 import os
-# ------------------------------------------
-print('Running: ', os.path.abspath(__file__))
-# ------------------------------------------
 # For accessing date and time
 from datetime import date
 from datetime import datetime
 now = datetime.now()
-# Print out dd/mm/YY H:M:S
-# ------------------------------------------
+
+# Print out dd/mm/YY H:M:S and own filepath
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 print("Started:", dt_string)
-# ------------------------------------------ 
-print("\n------> IMPORTING PACKAGES.... ---------------------------------------\n")
-print("-->Importing datasets...")
-# Import datasets and evaluation metric
-from datasets import load_dataset, load_metric, ClassLabel
-# Convert pandas dataframe to DatasetDict
-from datasets import Dataset
-# Generate random numbers
-print("-->Importing random...")
-import random
-# Manipulate dataframes and numbers
-print("-->Importing pandas & numpy...")
-import pandas as pd
-import numpy as np
-# Use regex
-print("-->Importing re...")
-import re
-# Read, Write, Open json files
-print("-->Importing json...")
-import json
-# Use models and tokenizers
-print("-->Importing Wav2VecCTC...")
-from transformers import Wav2Vec2CTCTokenizer
-from transformers import Wav2Vec2ForCTC
-from transformers import Wav2Vec2FeatureExtractor
-from transformers import Wav2Vec2Processor
-# Loading audio files
-print("-->Importing soundfile...")
-import soundfile as sf
-# For training
-print("-->Importing torch, dataclasses & typing...")
-import torch
-from dataclasses import dataclass, field
-from typing import Any, Dict, List, Optional, Union
-print("-->Importing from transformers for training...")
-from transformers import TrainingArguments
-from transformers import Trainer
-print("-->Importing pyarrow for loading dataset...")
-import pyarrow as pa
-import pyarrow.csv as csv
-print("-->SUCCESS! All packages imported.")
-
-# ------------------------------------------
-print('\nRunning: ', os.path.abspath(__file__))
-# ------------------------------------------
+print('\nRunning:', os.path.abspath(__file__))
 
 # ------------------------------------------
 #            Set data paths
 # ------------------------------------------
 print("\n------> Setting data paths -------------------------------------------\n")
-# Datatset filepath and name
-# OGI test set
-dataset_name = "output195.csv"
-# TLT17 test set
-#dataset_name = "output196.csv"
-# MyST test set
-#dataset_name = "output197.csv"
+# output195.txt = OGI test set
+# output196.txt = TLT17 test set 
+# output197.txt = MyST test set
+dataset_name = "output197.csv"
 
 # Crafting filepath
 dataset_fp = "/srv/scratch/z5160268/2020_TasteofResearch/kaldi/egs/renee_thesis/s5/wav2vec_projects/output/"+dataset_name
@@ -89,34 +44,18 @@ print("--> Dataset filepath:", dataset_fp)
 #           Reading in dataset
 # ------------------------------------------
 print("\n------> Reading in dataset... ----------------------------------------\n")
-# Reading in dataset1 dataframe from csv file, as string type
-# to preserve leading zeros in speaker id
 df = pd.read_csv(dataset_fp, dtype=str)
+# Convert all to string
+df['transcription_clean'] = df['transcription_clean'].astype(str)
+df['prediction'] = df['prediction'].astype(str)
 
-# Get the ground truth transcription and DeepSpeech hypothesis
-ground_truth = df['transcription_clean'].tolist()
-hypothesis = df['prediction'].tolist()
+# ------------------------------------------
+#            Computing WER
+# ------------------------------------------
+wer = load_metric("wer")
+#predictions = df['prediction'].tolist()
+#references = df['transcription_clean'].tolist()
+wer_score = wer.compute(predictions=df['prediction'], references=df['transcription_clean'])
 
-#error = wer(ground_truth[:50], hypothesis[:50])
+print("WER:", wer_score)
 
-wer = pywer.wer(references, hypotheses)
-cer = pywer.cer(references, hypotheses)
-
-print(wer)
-'''
-wer_metric = load_metric("wer")
-def compute_metrics(pred):
-    pred_logits = pred.predictions
-    pred_ids = np.argmax(pred_logits, axis=-1)
-
-    pred.label_ids[pred.label_ids == -100] = processor.tokenizer.pad_token_id
-
-    pred_str = processor.batch_decode(pred_ids)
-    # we do not want to group tokens when computing the metrics
-    label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
-
-    wer = wer_metric.compute(predictions=pred_str, references=label_str)
-
-    return {"wer": wer}
-print("SUCCESS: Defined WER evaluation metric.")
-'''
